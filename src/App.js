571b43126a91a14a0ab91cabeb7e896406572c1d
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-12-25 14:14:56 +0530

    feat: react app completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './components/Homepage/Home';
+import Details from './components/Detailspage/Details';
 
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Navigate to="/home" replace />}></Route>
+                <Route path="/home" element={<Home />} />
+                <Route path="/details/:id" element={<Details/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
